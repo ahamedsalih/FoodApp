7fc256a5b536b9c049f9a902695be64af2c417c6
unknown <ahamed.salih56@gmail.com>
2020-09-26 23:46:15 +0530

    Updates

---
+++ precache-manifest.ce90395f794af8a139b2e8d4c7528cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f59439668683342f86b98da02b21470",
+    "url": "/FoodApp/index.html"
+  },
+  {
+    "revision": "cabf8e633966ae2435ec",
+    "url": "/FoodApp/static/css/main.45df7bce.chunk.css"
+  },
+  {
+    "revision": "40e5a8c9dce9f0ca551c",
+    "url": "/FoodApp/static/js/2.76c1f93d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/FoodApp/static/js/2.76c1f93d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabf8e633966ae2435ec",
+    "url": "/FoodApp/static/js/main.20e2e315.chunk.js"
+  },
+  {
+    "revision": "676baeade8ca1c8544b8",
+    "url": "/FoodApp/static/js/runtime-main.c5cd9890.js"
+  }
+]);
